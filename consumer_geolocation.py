cdb865c462a343f59a470e68d8b0d4c2215ac6a5
kdb04 <nikhil.srivatsa1@gmail.com>
2025-04-19 15:35:11 +0530

    connection and metrics for geo_location is done

--- consumer_geolocation.py
+++ consumer_geolocation.py
@@ -2,8 +2,24 @@ from kafka import KafkaConsumer
 import json
 import re
 import time
+import os
+import psycopg2
+import psutil
+from dotenv import load_dotenv
 
-# Precompiled regex patterns
+load_dotenv()
+
+DB_CONFIG = {
+    'dbname': os.getenv('POSTGRES_DB'),
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'host': os.getenv('POSTGRES_HOST'),
+    'port': os.getenv('POSTGRES_PORT')
+}
+
+conn = psycopg2.connect(**DB_CONFIG)
+cursor = conn.cursor()
+
 delhi_pattern = re.compile(r"\b(new\s*)?delhi\b", re.IGNORECASE)
 mumbai_pattern = re.compile(r"\bmumbai\b", re.IGNORECASE)
 
@@ -11,9 +27,11 @@ consumer = KafkaConsumer(
     'GeoLocation',
     bootstrap_servers='localhost:9092',
     auto_offset_reset='earliest',
-    group_id=None,  # Removed group_id to receive all messages
+    group_id=None,
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+process = psutil.Process()
 
 delhi_count = 0
 mumbai_count = 0
@@ -23,6 +41,8 @@ print("-" * 50)
 
 try:
     for msg in consumer:
+        start_time = time.time()
+
         data = msg.value
         location = data.get("user_location", "").lower()
 
@@ -41,8 +61,27 @@ try:
         print(f"Date: {data['date']}")
         print(f"Current Counts - Delhi: {delhi_count} | Mumbai: {mumbai_count}")
         print("-" * 50)
-        time.sleep(5)  # Add delay between tweets
+
+        insert_query = """
+            INSERT INTO geo_location (user_name, text, location)
+            VALUES (%s, %s, %s)
+        """
+        cursor.execute(insert_query, (data['user_name'], data['text'], city))
+        conn.commit()
+        print("Tweet inserted into database")
+
+        end_time = time.time()
+        cpu = process.cpu_percent(interval=0.5)
+        mem = process.memory_info().rss / 1024**2
+
+        print("\nConsumer for geo-location tweets")
+        print(f"Execution Time: {end_time - start_time:.2f} sec")
+        print(f"CPU Usage: {cpu:.2f}%")
+        print(f"Memory Usage: {mem:.2f} MB")
+
+        time.sleep(5)
 
 except KeyboardInterrupt:
-    print("\nStopping consumer...")
+    print("\n Stopping consumer...")
     consumer.close()
+    conn.close()
